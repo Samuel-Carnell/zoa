27209db9c1ca16952d3e4ebbd1fc0a79d5d47ec5
Samuel Carnell <samuel.carnell@outlook.com>
2024-04-13 18:03:34 +0100

    fix: export zod openapi type extension

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 import 'setup';
 export * from './app';
 export * from './types';
+import {
+	ZodOpenAPIMetadata,
+	ZodOpenApiFullMetadata,
+} from '@asteasolutions/zod-to-openapi/dist/zod-extensions';
+import * as zod from 'zod';
+
+declare module 'zod' {
+	interface ZodTypeDef {
+		openapi?: ZodOpenApiFullMetadata;
+	}
+	interface ZodType<Output = any, Def extends ZodTypeDef = ZodTypeDef, Input = Output> {
+		openapi<T extends zod.ZodTypeAny>(
+			this: T,
+			metadata: Partial<ZodOpenAPIMetadata<zod.input<T>>>
+		): T;
+		openapi<T extends zod.ZodTypeAny>(
+			this: T,
+			refId: string,
+			metadata?: Partial<ZodOpenAPIMetadata<zod.input<T>>>
+		): T;
+	}
+}
